1806728baec480ecc8d27b49d0200261f07c6e18
asadmansr <asadmansr@gmail.com>
2019-10-17 09:13:50 -0400

    Added logic for exit code parsing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
+exit_code=0
 
 for i in `find . -name "TEST-*.xml" -type f`; do
     python extractReport.py "$i"
+    echo "python"
+    echo $?
+    if [ $? -ne 0 ]; then
+        exit_code=1
+        echo "check"
+        echo $exit_code
+    fi
 done
+echo "echo code final:"
+echo $exit_code
+exit $exit_code
--- extractReport.py
+++ extractReport.py
@@ -1,20 +1,31 @@
 import sys
 import xml.etree.ElementTree as ET
 
-def parseXML(xmlfile):
-    tree = ET.parse(xmlfile)
+SPACE_THRESHOLD = 16
+
+def parse_xml(xml_file):
+    exit_code = 0
+    tree = ET.parse(xml_file)
     root = tree.getroot()
     attributes = root.attrib
-    for k,v in attributes.items():
-        cs = len(k)
-        sp = 16-cs
-        print(k.capitalize() + " "*sp + v)
+    for key,value in attributes.items():
+        key_len = len(key)
+        empty_space = SPACE_THRESHOLD - key_len
+        print(key.capitalize() + " "*empty_space + value)
+        if key == 'failures' and int(value) > 0:
+            exit_code = 1
     print("")
+    print("internal)")
+    print(exit_code)
+    return exit_code
 
 def main():
     path = sys.argv[1]
     print(path + "\n")
-    parseXML(path)
+    exit_code = parse_xml(path)
+    print("exit codio:")
+    print(exit_code)
+    sys.exit(exit_code)
 
 if __name__ == "__main__":
     main()
