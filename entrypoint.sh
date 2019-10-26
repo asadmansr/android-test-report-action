859eff7dc8a992803c80be8ead5967d0d329088f
asadmansr <asadmansr@gmail.com>
2019-10-26 09:41:09 -0400

    Updating status code in bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 exit_code=0
 for i in `find . -name "TEST-*.xml" -type f`; do
     python extractReport.py "$i"
-    result=$(cat android_test_report_action.txt)
-    if [ $result -ne 0 ]; then
-        exit_code=$result
-    fi
+    echo $?
+    #result=$(cat android_test_report_action.txt)
+    #if [ $result -ne 0 ]; then
+    #    exit_code=$result
+    #fi
 done
-echo $exit_code
-exit $exit_code
+exit 1
