1d50bd4070ed992dc878d4960567cf0ab2a25680
asadmansr <asadmansr@gmail.com>
2020-05-02 17:22:46 -0400

    Added logic to fail action when unit tests were not executed successfully due to compile error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 status_code=0
+num_files=0
 log_file='extractReport_status.log'
 status_message='Android Test Report Action executed successfully.'
 
@@ -14,9 +15,15 @@ echo ''
 touch $log_file
 
 for i in `find . -name "TEST-*.xml" -type f`; do
+    let num_files=num_files+1
     python /usr/bin/extractReport.py "$i"
     echo ''
 done
+
+if [ "$num_files" -eq "0" ]; then
+   status_code=1
+   status_message='No test reports found. Please verify the tests were executed successfully. Android Test Report Action failed the job.'
+fi
 
 if grep -q 'error' "$log_file"; then
   status_code=1
